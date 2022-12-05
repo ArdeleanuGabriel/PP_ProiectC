e7abd02a4c337c6a7c9ac51efadf8fda97666058
ARDELEANU Gabriel <79141575+ArdeleanuGabriel@users.noreply.github.com>
2022-12-05 15:51:07 +0200

    Modificarile la proiect + cerinta bonus
    Am facut modificarile discutate la laborator dupa care am facut modificari pentru cerinta bonus "Pentru fiecare cuvant comun sa se afiseze numarul de aparitii"

--- ProiectC/Proiect_main.py
+++ ProiectC/Proiect_main.py
@@ -1,5 +1,12 @@
 import sys
 from commom_phrases import *
 
+
+
 if __name__ == "__main__":
-    print(Show_common_phrases(sys.argv[1],sys.argv[2]))
+    if len(sys.argv) < 3:
+        print("Not enough arguments")
+    elif len(sys.argv) > 3:
+        print("Too many arguments")
+    elif  len(sys.argv) == 3:
+        print(Show_common_phrases(sys.argv[1],sys.argv[2]))
--- ProiectC/commom_phrases.py
+++ ProiectC/commom_phrases.py
@@ -9,9 +9,9 @@ def Show_common_phrases(file1,file2):
         
     #Citirea frazelor din fisierele text
     f = open(file1,"r")
-    x=f.read()
+    x=f.read().lower()
     f = open(file2,"r")
-    y=f.read()
+    y=f.read().lower()
     #printab(x,y)
     #Inlaturam spatiile in exces din jurul frazelor citite
     x = x.strip()
@@ -19,28 +19,38 @@ def Show_common_phrases(file1,file2):
     #printab(x,y)
     #Inlaturarea spatiilor in exces din jurul punctelor
     x = re.sub(r'\s*\.\s*','.',x)
+    x = re.sub(r'\s*\!\s*','!',x)
+    x = re.sub(r'\s*\?\s*','?',x)
     y = re.sub(r'\s*\.\s*','.',y)
+    y = re.sub(r'\s*\!\s*','!',y)
+    y = re.sub(r'\s*\?\s*','?',y)
     #printab(x,y)
     #Inlaturarea spatiilor in exces dintre cuvinte
     x = re.sub(r'\s+',' ',x)
     y = re.sub(r'\s+',' ',y)
     #printab(x,y)
     #Impartirea frazelor in propozitii (in lista exista posibilitatea sa intalnim elemente goale <" ">)
-    x = x.split('.')
-    y = y.split('.')
+    #x = x.split('.')
+    #y = y.split('.')
+    x = re.split('[.]|!|[?]| ', x)
+    y = re.split('[.]|!|[?]| ', y)
     #printab(x,y)
     #Transformama din liste in set pentru a scapa de eventuale propozitii repetate
-    x = set(x)
-    y = set(y)
+    x_set = set(x)
+    y_set = set(y)
     #printab(x,y)
 
-    prop_comune = []
-    for i in x:
-        i=i.lower()
-        if i not in prop_comune and i != '': #Daca nu verificam element gol si daca nu l-am validat deja.
-            for j in y:
-                j = j.lower()
-                if i == j:
-                    prop_comune.append(i.capitalize() + ".")
+    prop_comune = x_set.intersection(y_set)
+    Propozitii = []
+    for i in prop_comune:
+        if i != '':
+            Propozitii.append(i)
 
-    return(prop_comune)
+    for i in Propozitii:
+        counter=0
+        for j in x:
+            if i == j:
+                counter+=1
+        print("Cuvant " + str(i) + " Counter " + str(counter))
+    
+    return(Propozitii)
